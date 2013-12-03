312d566d4a6d5d9ef08d05db531fdc82bfa0238e
stephendiadamo <stephen.diadamo@gmail.com>
2013-12-03 12:44:58 +1200

    Bug fix

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -163,7 +163,7 @@ class main extends CI_Controller {
 		if (isset($uid) && isset($cost) && isset($start) && isset($duration)){
 			$this->load->model("connector");
 			// TODO: convert start to day
-			$result = $this->connector->addDailyEntry($uid, $start, $duration, $cost);			
+			$result = $this->connector->addDailyEntry($uid, $cost, $start, $duration);			
 		} else {
 			return;
 		}
